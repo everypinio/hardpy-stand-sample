7be9c52619aa98701556649b0ee21248de37c047
i.alexandrov <i.alexandrov@thirdpin.io>
2024-08-13 08:20:23 +0300

    [tests] Add numeric input to test led

--- tests/test_2_base_functions.py
+++ tests/test_2_base_functions.py
@@ -1,5 +1,9 @@
 import pytest
 import hardpy
+from hardpy.pytest_hardpy.utils.dialog_box import (
+    DialogBoxWidget,
+    DialogBoxWidgetType,
+)
 
 from dut_driver import DutDriver
 
@@ -26,6 +30,14 @@ def test_serial_num(device_under_test: DutDriver):
 
 @pytest.mark.case_name("LED")
 def test_jumper_closed(device_under_test: DutDriver):
-    assert (
-        device_under_test.req_jumper_status() == 0
-    ), "LED does not light up, check the jumper"
+    hardpy.set_message("Сount the number of LED blinks")
+    jumper_status = device_under_test.req_jumper_status()
+    assert jumper_status == 0, "LED does not light up, check the jumper"
+
+    dbx = hardpy.dialog_box.DialogBox(
+        dialog_text="Enter the number of LED blinks",
+        title_bar="LED check",
+        widget=DialogBoxWidget(DialogBoxWidgetType.NUMERIC_INPUT),
+    )
+    user_input = int(hardpy.run_dialog_box(dbx))
+    assert 3 == user_input, f"The LED blinked 10 times, user entered {user_input}"
